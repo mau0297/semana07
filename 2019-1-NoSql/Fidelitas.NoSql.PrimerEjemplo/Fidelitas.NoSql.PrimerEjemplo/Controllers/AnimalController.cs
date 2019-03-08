2cf32f2717b269c83b557bdfe138ef8be213da18
unknown <mau0297@gmail.com>
2019-03-07 18:28:13 -0600

    adde last proyect

---
+++ 2019-1-NoSql/Fidelitas.NoSql.PrimerEjemplo/Fidelitas.NoSql.PrimerEjemplo/Controllers/AnimalController.cs
@@ -0,0 +1,123 @@
+﻿using Fidelitas.NoSql.PrimerEjemplo.Collections;
+using Fidelitas.NoSql.PrimerEjemplo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fidelitas.NoSql.PrimerEjemplo.Controllers
+{
+    public class AnimalController : Controller
+    {
+        /*
+         * {
+    "_id" : ObjectId("5c7890162e132d0a74ea1316"),
+    "Nombre" : "firulais",
+    "fecha" : ISODate("2005-01-01T06:00:00.000Z"),
+    "Tipo" : "hamster",
+    "Dueno" : "pepito",
+    "Peso" : 5,
+    "tratamiento" : null
+}
+
+            {
+    "_id" : ObjectId("5c7893ec2e132d045802429d"),
+    "Nombre" : "Chita",
+    "fecha" : ISODate("1989-01-01T06:00:00.000Z"),
+    "Tipo" : "Chimpancé",
+    "Dueno" : "Tarzán",
+    "Peso" : 10,
+    "tratamiento" : null
+}
+         * 
+         */
+
+        private ContextoMongo elContexto = new ContextoMongo();
+
+
+        // GET: Animal
+        public ActionResult Index()
+        {
+            var animales = elContexto.LosAnimales;
+            var losAnimalitos = animales.AsQueryable().ToList();
+            return View(losAnimalitos);
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Animal/Create
+        public ActionResult Insertar()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        public ActionResult Insertar(Animales laMascota)
+        {
+            try
+            {
+                var animales = elContexto.LosAnimales;
+                animales.InsertOne(laMascota);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
